10054ded0deec7dcff8488716ce8f6a93bec53df
Benjamin Teuber <bsteuber@gmail.com>
2016-03-16 21:24:17 +0100

    Board: Stones and marks, react diffing

--- src/learngo/board.cljs
+++ src/learngo/board.cljs
@@ -1,7 +1,40 @@
 (ns learngo.board
-  (:require [reagent.core :as r]))
+  (:require [clojure.data :as data]
+            [reagent.core :as r]))
 
 (enable-console-print!)
+
+(defn make-object [[x y] item]
+  (let [pos-map {:x x
+                 :y y}
+        item-map (cond (= item :black)  {:c js/WGo.B}
+                       (= item :white)  {:c js/WGo.W}
+                       (= item :circle) {:type "CR"}
+                       (string? item)   {:type "LB" :text item})]
+    (clj->js
+     (merge pos-map item-map))))
+
+(defn make-objects [diff-map]
+  (->> diff-map
+       (map (fn [[pos item]]
+              (make-object pos item)))
+       clj->js))
+
+(defn remove-all [bd diff-map]
+  (.removeObject bd (make-objects diff-map)))
+
+(defn add-all [bd diff-map]
+  (.addObject bd (make-objects diff-map)))
+
+(defn update-board [bd last-state current-state]
+  (let [[stones-to-remove stones-to-add] (data/diff (:stones last-state)
+                                                    (:stones current-state))
+        [marks-to-remove marks-to-add] (data/diff (:marks last-state)
+                                                  (:marks current-state))]
+    (remove-all bd stones-to-remove)
+    (remove-all bd marks-to-remove)
+    (add-all bd stones-to-add)
+    (add-all bd marks-to-add)))
 
 (defn board [{:keys [size width height top left right bottom]
               :or {size 19
@@ -10,20 +43,28 @@
                    right 0
                    bottom 0}}
              state]
-  (let [bd-ref (atom nil)]
-    [(r/create-class
-      {:component-did-mount
-       (fn [obj]
-         (let [node (js/ReactDOM.findDOMNode obj)
-               bd (js/WGo.Board. node
-                                 (clj->js {:height height
-                                           :width width
-                                           :size size
-                                           :section (clj->js {:top top
-                                                              :left left
-                                                              :right right
-                                                              :bottom bottom})}))]
-           (reset! bd-ref bd)))
-       :reagent-render
-       (fn [state]
-         [:div.board])})]))
+  (let [bd-ref (atom nil)
+        last-state (atom nil)]
+    (r/create-class
+     {:component-did-mount
+      (fn [obj]
+        (let [node (js/ReactDOM.findDOMNode obj)
+              bd (js/WGo.Board. node
+                                (clj->js {:height height
+                                          :width width
+                                          :size size
+                                          :section (clj->js {:top top
+                                                             :left left
+                                                             :right right
+                                                             :bottom bottom})}))]
+          (reset! bd-ref bd)
+          (update-board bd nil @state)
+          (reset! last-state @state)))
+      :component-will-update
+      (fn [_ [_ _ state]]
+        (update-board @bd-ref @last-state @state)
+        (reset! last-state @state))
+      :reagent-render
+      (fn [init state]
+        @state
+        [:div.board])})))
--- test/learngo/board_test.cljs
+++ test/learngo/board_test.cljs
@@ -2,18 +2,33 @@
   (:require [cljs.test     :refer-macros [is]]
             [devcards.core :refer-macros [defcard-rg
                                            deftest
-                                           start-devcard-ui!]]
+                                          start-devcard-ui!]]
             [learngo.board :as bd]
             [reagent.core  :as r]))
 
+(def sample-state
+  (atom {:stones {[14 14] :black
+                  [14 16] :white}}))
+
 (defcard-rg board
-  (bd/board {:size 19
+  [bd/board {:size 19
              :width 400}
-            nil))
+   (atom nil)])
 
 (defcard-rg partial
-  (bd/board {:size 19
+  [bd/board {:size 19
              :width 300
              :top 7
              :left 7}
-            nil))
+   sample-state])
+
+(defcard-rg marks
+  [bd/board {:size 19
+             :width 300
+             :top 7
+             :left 7}
+   (atom {:stones {[15 15] :black
+                   [15 16] :white}
+          :marks {[15 15] "A"
+                  [15 16] :circle
+                  [15 17] "B"}})])
